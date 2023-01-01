c4b84213fdb8ef20da03355c7105033ab4c14cfc
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-01-01 17:02:27 -0300

    criando servidor

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import aboutImg from '../assets/images/about-image.jpeg'
+
+export const About = () => {
+  return (
+    <div className='bg-white'>
+      <div className='p-24 grid grid-cols-2'>
+        <div className="">
+          <h2 className="text-2xl font-medium ">Sobre Nós</h2>
+          <p className='text-lg'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A unde iste doloremque quidem officiis maxime, dolore ex laudantium ut obcaecati, autem, repellendus repudiandae quasi. Dolorum reiciendis dolore non vel illum? 
+          </p>
+        </div>
+        <div className='flex items-center justify-center'>
+          <img src={aboutImg} alt="" className='w-[400px] h-[400px] object-cover' />
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import clsx from 'clsx'
 
-export const Button = ({children}) => {
+function getClassName({className}){
+  return clsx (
+    'text-gray-200 text-lg font-bold rounded-xl transition duration-300 cursor-pointer focus:outline-none focus:ring-2 focus:ring-opacity-50 hover:text-white ',
+    className
+  )
+}
+
+const sizes = {
+  small: 'px4 py-3',
+  medium: 'px-6 py-4',
+  large: 'w-full px-4 py-3 ',
+}
+const variants = {
+  primary: 'bg-marigold focus:ring-marigold',
+  secondary: 'bg-tomato focus:ring-tomato',
+  dark: 'bg-black focus:ring-white',
+}
+export const Button = ({
+  children,
+  className,
+  size= 'small',
+  variant= 'primary',
+  ...rest
+
+}) => {
   return (
-    <button>
+    <button className={clsx(
+      sizes[size],
+      variants[variant],
+      getClassName({className})
+    )}
+    {...rest}>
         {children}
     </button>
   )
